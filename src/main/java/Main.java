5da994c15b5a8306b5d7318ea25d708cdbf42bfa
JivanSalunke <jsalunke7197@gmail.com>
2024-01-04 21:39:05 +0530

    restuructured main file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,15 +10,21 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        fileIO f=new fileIO();
-        System.out.println("Hello world!");
-//        f.read_Sort_Write("C:/Users/JSA334/IdeaProjects/java-basics/files/in1.txt","C:/Users/JSA334/IdeaProjects/java-basics/files/in2.txt");
-        RestClient restClient= new RestClient();
-        Response<List<PhotoResponse>> response = restClient.getJsonPlaceholderAPI().getPhotos().execute();
-
-        response.body().forEach(photo->{
-            System.out.println(photo.getTitle());
-        });
+        System.out.println("hello world");
+//        ex:1 using okHttpClient
+            /*
+            fileIO f=new fileIO();
+            f.read_Sort_Write("C:/Users/JSA334/IdeaProjects/java-basics/files/in1.txt","C:/Users/JSA334/IdeaProjects/java-basics/files/in2.txt");
+            */
+        
+//        ex:2 using Retrofit
+            /*
+            RestClient restClient= new RestClient();
+            Response<List<PhotoResponse>> response = restClient.getJsonPlaceholderAPI().getPhotos().execute();
+            response.body().forEach(photo->{
+                System.out.println(photo.getTitle());
+            });
+            */
     }
 
 }
